501f96dbabef33be0f6dcce817a2f0927cad1221
Stanley Huang <infamous_godhand@yahoo.com>
2020-07-21 18:58:02 +1000

    implement knn

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,21 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  console.log(outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  _.chain(outputs)
+    .map((row) => [distance(row[0]), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, k);
 }
-
